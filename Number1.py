263b33f4ae7a4d7c3bb05c75fa626969dede7e86
Dmitry <123157243+Verchansky@users.noreply.github.com>
2023-02-01 11:21:20 +0300

    Update Number1.py
    Изменено условие в функции CheckNumber

--- Number1.py
+++ Number1.py
@@ -36,7 +36,7 @@ def versatileTriangle(a, b, c):
 # Проверка условия
 def checkNumber(a, b, c):
     status = bool(False)
-    if (a + b >= c and a + c >= b and b + c >= a) and (a > 0 and b > 0 and c > 0):
+    if (a + b > c and a + c > b and b + c > a) and (a > 0 and b > 0 and c > 0):
         status = True
         return status
     else:
